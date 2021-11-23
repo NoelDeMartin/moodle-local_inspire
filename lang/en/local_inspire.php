2c2718cbbdbd39efeb0fff2f9ba98d532a0f5bce
Noel De Martin <noel@moodle.com>
2021-11-23 16:40:28 +0100

    Implement privacy provider

--- lang/en/local_inspire.php
+++ lang/en/local_inspire.php
@@ -4,4 +4,5 @@ $string['inspire_dynamic'] = 'Inspire (dynamic)';
 $string['inspire_static'] = 'Inspire (static)';
 $string['inspire'] = 'Inspire';
 $string['pluginname'] = 'Inspire';
+$string['privacy_metadata'] = 'This plugin does not store any user data';
 $string['refresh'] = 'Refresh';
